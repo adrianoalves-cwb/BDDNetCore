3d9ae1a82a1d9b473c66fda7087b84ec82700e32
adrianoalves-cwb <adrianoalves.cwb@gmail.com>
2020-09-06 12:09:54 -0300

    GREEN

--- BDDNetCoreConsole/CalculoHora.cs
+++ BDDNetCoreConsole/CalculoHora.cs
@@ -6,7 +6,27 @@ namespace BDDNetCoreConsole
 {
     public class CalculoHora
     {
+		private double _salario;
 
-        //teste
+		public CalculoHora(double salario) =>
+			_salario = salario;
+
+		public CalculoHora() =>
+			_salario = 3000;
+
+		public double CalculaSalarioHora() =>
+			_salario / 220;
+
+		public double CalculaValorHoraExtraNormal() =>
+			CalculaSalarioHora() * 1.5;
+
+		public double CalculaValorHoraExtraRemunerada() =>
+			CalculaSalarioHora() * 2;
+
+		public double CalculaHoraExtraNormal(double quantidade) =>
+			CalculaValorHoraExtraNormal() * quantidade;
+
+		public double CalculaHoraExtraRemunerada(double quantidade) =>
+			CalculaValorHoraExtraRemunerada() * quantidade;
 	}
 }
